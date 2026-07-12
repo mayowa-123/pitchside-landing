a041e2356e8a93b09705a8e24d62fc73689d1d86
Mayor-tech <mayowaabass2009@gmail.com>
2026-07-12 20:18:58 +0100

    Create stats.jsx

---
+++ component/stats.jsx
@@ -0,0 +1,50 @@
+function Stats(){
+
+return(
+
+<section className="py-20 text-center">
+
+<h2 className="text-4xl font-bold">
+Built For The Future Of Football
+</h2>
+
+<div className="grid md:grid-cols-3 gap-8 mt-10 max-w-5xl mx-auto">
+
+<div>
+<h3 className="text-4xl text-green-400 font-bold">
+AI
+</h3>
+<p className="text-gray-400">
+Football Intelligence
+</p>
+</div>
+
+
+<div>
+<h3 className="text-4xl text-green-400 font-bold">
+Data
+</h3>
+<p className="text-gray-400">
+Player Analytics
+</p>
+</div>
+
+
+<div>
+<h3 className="text-4xl text-green-400 font-bold">
+Fans
+</h3>
+<p className="text-gray-400">
+Global Community
+</p>
+</div>
+
+</div>
+
+</section>
+
+)
+
+}
+
+export default Stats;
