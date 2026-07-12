adef94840f586b2ac74fef7e1f62718516885236
Mayor-tech <mayowaabass2009@gmail.com>
2026-07-12 21:00:16 +0100

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Stats from "./components/Stats";
-import Roadmap from "./components/Roadmap";
-import Waitlist from "./components/Waitlist";
-import Footer from "./components/Footer";
+import Navbar from "./component/navbar";
+import Hero from "./component/hero";
+import Features from "./component/features";
+import Stats from "./component/stats";
+import Roadmap from "./component/roadmap";
+import Waitlist from "./component/waitlist";
+import Footer from "./component/footer";
 
 function App() {
   return (
