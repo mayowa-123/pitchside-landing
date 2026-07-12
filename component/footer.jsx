c229b6520b9b8def331131af61f07df70e5c313b
Mayor-tech <mayowaabass2009@gmail.com>
2026-07-12 20:21:11 +0100

    Create footer.jsx

---
+++ component/footer.jsx
@@ -0,0 +1,21 @@
+function Footer(){
+
+return(
+
+<footer className="py-10 text-center border-t border-gray-900">
+
+<h2 className="text-2xl font-bold text-green-400">
+Pitchside
+</h2>
+
+<p className="text-gray-500 mt-3">
+The Football Social App For Real Fans.
+</p>
+
+</footer>
+
+)
+
+}
+
+export default Footer;
