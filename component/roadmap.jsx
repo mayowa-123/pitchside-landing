38a79105da1f207ec7f1fbddbc3a948892fd605b
Mayor-tech <mayowaabass2009@gmail.com>
2026-07-12 20:20:00 +0100

    Create roadmap.jsx

---
+++ component/roadmap.jsx
@@ -0,0 +1,27 @@
+function Roadmap(){
+
+return(
+
+<section className="px-6 py-20">
+
+<h2 className="text-4xl font-bold text-center">
+Roadmap
+</h2>
+
+
+<div className="max-w-xl mx-auto mt-10 space-y-5">
+
+<p>✅ MVP Development</p>
+<p>🚧 Private Beta Launch</p>
+<p>📊 AI & Player Analytics Expansion</p>
+<p>🌍 Global Football Community</p>
+
+</div>
+
+</section>
+
+)
+
+}
+
+export default Roadmap;
